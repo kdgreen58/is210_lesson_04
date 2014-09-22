1ae4b58f6100d830ccc44bf50f92e24ccb739d91
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-22 02:02:25 -0400

    My task 01 code

---
+++ task_01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A for loop for a Shakespeare play."""
+
+import data
+
+print data.SHAKESPEARE
+
+
+RAWDATA = data.SHAKESPEARE.split("\n")
+
+MAXIMUM_WORDS = None
+
+for number in range (0, len(RAWDATA)):
+    RAWDATAT = len(RAWDATA[number].split(" "))
+    if RAWDATAT > MAXIMUM_WORDS:
+        MAXIMUM_WORDS = RAWDATAT
+        
+MINIMUM_WORDS = MAXIMUM_WORDS
+
+for numbertwo in range (0, len(RAWDATA)):
+    RAWDATATT = len(RAWDATA[numbertwo].split(" "))
+    if RAWDATATT < MINIMUM_WORDS:
+        MINIMUM_WORDS = RAWDATATT
+
+AVERAGE_WORDS = float(len(data.SHAKESPEARE.split(" ")) / len(RAWDATA))
+
+DATAREMOVE = data.SHAKESPEARE.replace(".", " ").replace("'", " ").replace(";"
+                 , " ").replace("-", " ").replace("?", " ").replace(",", " ")
+print DATAREMOVE
+RAWDATATWO = DATAREMOVE.split()
+NUM_CRISPIAN = RAWDATATWO.count("Crispian")
+    
+
+
+

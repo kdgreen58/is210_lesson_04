505dcb7119deba1d2dcb017dad7d829169ec2c83
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-22 02:14:51 -0400

    My task 01 code

--- task_01.py
+++ task_01.py
@@ -23,7 +23,7 @@ for numbertwo in range (0, len(RAWDATA)):
     if RAWDATATT < MINIMUM_WORDS:
         MINIMUM_WORDS = RAWDATATT
 
-AVERAGE_WORDS = float(len(data.SHAKESPEARE.split(" ")) / len(RAWDATA))
+AVERAGE_WORDS = float(len(data.SHAKESPEARE.split())) / float(len(RAWDATA))
 
 DATAREMOVE = data.SHAKESPEARE.replace(".", " ").replace("'", " ").replace(";"
                  , " ").replace("-", " ").replace("?", " ").replace(",", " ")

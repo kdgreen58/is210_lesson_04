57aa6af9f4065124f5f32e4f741e2a27e0a6a235
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-22 02:17:27 -0400

    My task 01 code

--- task_01.py
+++ task_01.py
@@ -11,26 +11,22 @@ RAWDATA = data.SHAKESPEARE.split("\n")
 
 MAXIMUM_WORDS = None
 
-for number in range (0, len(RAWDATA)):
+for number in range(0, len(RAWDATA)):
     RAWDATAT = len(RAWDATA[number].split(" "))
     if RAWDATAT > MAXIMUM_WORDS:
         MAXIMUM_WORDS = RAWDATAT
-        
+
 MINIMUM_WORDS = MAXIMUM_WORDS
 
-for numbertwo in range (0, len(RAWDATA)):
+for numbertwo in range(0, len(RAWDATA)):
     RAWDATATT = len(RAWDATA[numbertwo].split(" "))
     if RAWDATATT < MINIMUM_WORDS:
         MINIMUM_WORDS = RAWDATATT
 
 AVERAGE_WORDS = float(len(data.SHAKESPEARE.split())) / float(len(RAWDATA))
 
-DATAREMOVE = data.SHAKESPEARE.replace(".", " ").replace("'", " ").replace(";"
-                 , " ").replace("-", " ").replace("?", " ").replace(",", " ")
+DATAREMOVE = data.SHAKESPEARE.replace(".", " ").replace("'", " ").replace(";",
+                                                                          " ").replace("-", " ").replace("?", " ").replace(",", " ")
 print DATAREMOVE
 RAWDATATWO = DATAREMOVE.split()
 NUM_CRISPIAN = RAWDATATWO.count("Crispian")
-    
-
-
-

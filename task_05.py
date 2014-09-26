4594a306f5afde41d223e00beafd72f9cad444e5
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-26 00:53:14 -0400

    My task 05 code

---
+++ task_05.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 5 Code"""
+
+import data
+
+MATCHUPS = ""
+COUNTER = 0
+LINE = '{0}, "{1}", "{2}"\n'
+
+for ROW_INDEX, ROW in enumerate(data.VERSUS):
+    for COLUMN_INDEX, COLUMN in enumerate(data.VERSUS):
+        if ROW_INDEX < COLUMN_INDEX:
+            COUNTER += 1
+            MATCHUPS += LINE.format(COUNTER, ROW, COLUMN)
+
+print MATCHUPS.strip()

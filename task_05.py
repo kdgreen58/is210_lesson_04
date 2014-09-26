95e255e09020bb168a3ffacfd115814709d86584
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-26 01:14:55 -0400

    My task 05 code

--- task_05.py
+++ task_05.py
@@ -12,6 +12,6 @@ for ROW_INDEX, ROW in enumerate(data.VERSUS):
     for COLUMN_INDEX, COLUMN in enumerate(data.VERSUS):
         if ROW_INDEX < COLUMN_INDEX:
             COUNTER += 1
-            MATCHUPS += LINE.format(COUNTER, ROW, COLUMN)
+            MATCHUPS += LINE.format(COUNTER, COLUMN, ROW)
 
 print MATCHUPS.strip()
